e8bd893cfcaaaf8b96e041e1c647c5d653861e76
flaviomarcilio <flaviomarciliodeoliveira@gmail.com>
2020-12-05 10:43:36 -0300

    Cria um formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,6 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { MaterialModule } from './shared/material/material.module';
 
-import { FilmesModule } from './filmes/filmes.module';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,8 +22,7 @@ import { FilmesModule } from './filmes/filmes.module';
     BrowserAnimationsModule,
     LayoutModule,
     MaterialModule,
-    AppRoutingModule,
-    FilmesModule
+    AppRoutingModule
   ],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt'}],
   bootstrap: [AppComponent]
